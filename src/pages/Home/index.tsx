04a9efd419a631a89169adf9181e21b21e7b159c
CitrusSoda <dndhakus1004@naver.com>
2024-06-05 14:59:17 +0900

    ✨ feat: 유저 이름 출력

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,31 @@
+import { useAtomValue } from 'jotai';
 import { AuthUser } from '../../components/AuthUser';
 import Greet from '../../components/Greet';
 import Idols from '../../components/Idols';
 import styles from './style';
+import { sessionAtom } from '../../stores/AuthStore';
+import { supabase } from '../../supabaseClient';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
+  const session = useAtomValue(sessionAtom);
+  const [userEmail, setUserEmail] = useState('');
+
+  async function getUserEmail() {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    setUserEmail(user?.email as string);
+  }
+
+  useEffect(() => {
+    getUserEmail();
+  }, []);
+
   return (
     <div css={styles.main}>
-      <Greet />
+      {session ? <Greet name={userEmail} /> : <Greet />}
       <AuthUser />
       <Idols />
     </div>
